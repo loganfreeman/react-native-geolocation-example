64a52d2841ff8f516f3e728f40fa2c222a0d7955
shanhong cheng <shnahongcheng@gmail.com>
2016-11-27 22:10:18 -0700

    initial commit

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/FeedReaderContract.java
@@ -0,0 +1,23 @@
+package com.example.myfirstapp;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by scheng on 11/24/16.
+ */
+
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+}
+
+
+
